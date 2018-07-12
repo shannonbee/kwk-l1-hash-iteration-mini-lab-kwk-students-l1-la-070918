21aeb77b27c3a2b0c11c62693daea5c8df484c5a
shannonbee <shannonbenyamin@gmail.com>
2018-07-12 16:29:46 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,6 +1,6 @@
 
-def olympics_hash 
- summer_olympics
+def summer_olympics 
+ summer_olympics =
   {"Sydney"=> 2000,"Athens"=>2004,"Beijing"=>2008,"London"=>2012}
   puts create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
